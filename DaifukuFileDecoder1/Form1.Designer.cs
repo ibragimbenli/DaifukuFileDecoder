d6ddc5720ad735ef6584942575525955a3686d30
ibrahim.benli <ibrahim.benli@geos.com.tr>
2023-10-18 15:24:35 +0300

    Add project files.

---
+++ DaifukuFileDecoder1/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace DaifukuFileDecoder1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnfileSec = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnfileSec
+            // 
+            this.btnfileSec.Location = new System.Drawing.Point(12, 9);
+            this.btnfileSec.Name = "btnfileSec";
+            this.btnfileSec.Size = new System.Drawing.Size(663, 62);
+            this.btnfileSec.TabIndex = 0;
+            this.btnfileSec.Text = "Dosya Aç";
+            this.btnfileSec.UseVisualStyleBackColor = true;
+            this.btnfileSec.Click += new System.EventHandler(this.btnfileSec_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(224, 244);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(663, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 129);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(1422, 424);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 103);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(663, 20);
+            this.textBox4.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1461, 565);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnfileSec);
+            this.Name = "Form1";
+            this.Text = "Daifuku";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnfileSec;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ DaifukuFileDecoder1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DaifukuFileDecoder1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnfileSec_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Text File| *.log";
+            OpenFileDialog Acdosya = new OpenFileDialog();
+            Acdosya.ShowDialog();
+            textBox1.Text = Acdosya.FileName;
+
+            if (Acdosya.FileName == null || Acdosya.FileName == "") return;
+
+            using (StreamReader Okunan = new StreamReader(Acdosya.FileName))
+            {
+                //File.AppendAllText(@"C:\Users\ibrahim.benli\Desktop\yaz.txt", Environment.NewLine);
+                string Dosyaicrk = Okunan.ReadToEnd().Replace("\n", "");
+                Dosyaicrk = Dosyaicrk.Replace(" ", " ").Replace("\r", "");
+                
+                textBox2.Text = Dosyaicrk;
+                textBox4.Text = Dosyaicrk;
+                int Sti = -1, Adet = 8, Say = 1;
+                string KntrlWrd1 = "", KntrlWrd2 = "06 07 81";
+                do
+                {
+                    Sti++;
+                    if (Dosyaicrk.Length < Sti + 8) 
+                        return;
+
+                    KntrlWrd1 = Dosyaicrk.Substring(Sti, Adet);
+
+                    if (KntrlWrd1 == KntrlWrd2)
+                    {
+                        Adet = Adet + 24;
+                        KntrlWrd1 = Dosyaicrk.Substring(Sti, Adet);
+                        Sti += 32;
+
+                        MessageBox.Show(KntrlWrd1 + " Adt: " + Adet + " Sti: " + Sti + " Say:" + Say);
+                        
+                        Adet = 8;
+                        Say++;
+                    }
+                } while (KntrlWrd1 != "End log f");
+            }
+
+        }
+    }
+}
